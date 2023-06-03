5c68c68c9aa9af8149cc6833526a87c87af2cf71
moskau-capital <umishunta@gmail.com>
2023-06-03 20:05:50 +0900

    最終更新日の表示欄を作(2)

--- WriteLastUpdatedDate.ts
+++ WriteLastUpdatedDate.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 
 const date = new Date();
 const dateString = date.toLocaleDateString();
-const fullString = `${dateString} ${date.getHours()}:${date.getMinutes()}`;
+const timeString = date.toLocaleTimeString().slice(0, -3);
+const fullString = `${dateString} ${timeString}`;
 
 fs.writeFileSync("./public/lastUpdatedDate.txt", fullString, "utf-8");
